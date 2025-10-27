c01cf56cf902a3630bfc297f6ef083606ff9f2e1
Rishabh Vemuri <rishabhsvemuri@gmail.com>
2025-10-26 20:30:28 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://rishabhsvemuri.github.io",
-  base: "/website",
   vite: {
     plugins: [tailwindcss()],
   },
